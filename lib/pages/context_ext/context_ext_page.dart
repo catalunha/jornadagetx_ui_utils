84544f73f1f0d1e4be1028bc54dacd709c30cda9
catalunha <catalunha.mj@gmail.com>
2022-10-03 20:51:50 -0300

    rev

--- lib/pages/context_ext/context_ext_page.dart
+++ lib/pages/context_ext/context_ext_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ContextExtPage extends StatelessWidget {
   const ContextExtPage({Key? key}) : super(key: key);
@@ -10,14 +11,58 @@ class ContextExtPage extends StatelessWidget {
         title: const Text('Context Extensions'),
       ),
       body: Center(
-        child: Column(children: [
-          Container(
-            // width: 200,
-            width: MediaQuery.of(context).size.width,
-            height: 200,
-            color: Colors.green,
-          )
-        ]),
+        child: SingleChildScrollView(
+          child: Column(children: [
+            Text('WxH: ${context.width.toInt()} x ${context.height.toInt()}'),
+            Text('isSmallTablet: ${context.isSmallTablet}'),
+            Text('isTablet: ${context.isTablet}'),
+            Text('isLargeTablet (Desktop): ${context.isLargeTablet}'),
+            Text('isPhone: ${context.isPhone}'),
+            Container(
+              width: 200,
+              height: 100,
+              color: Colors.cyan,
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              color: Colors.yellow,
+              // color: Colors.green,
+            ),
+            Container(
+              width: context.width,
+              height: 100,
+              color: context.theme.primaryColor,
+            ),
+            Container(
+              width: Get.width,
+              height: 100,
+              color: Get.theme.primaryColor,
+            ),
+            Container(
+              width: context.widthTransformer(dividedBy: 3),
+              height: 100,
+              color: Colors.amber,
+            ),
+            Container(
+              width: context.widthTransformer(reducedBy: 20),
+              height: 100,
+              color: Colors.black,
+            ),
+            context.responsiveValue(
+              desktop: Container(
+                width: 100,
+                height: 100,
+                color: Colors.black26,
+              ),
+              mobile: Container(
+                width: 100,
+                height: 100,
+                color: Colors.brown,
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
---
+++ lib/pages/getview/getview_page.dart
@@ -0,0 +1,1 @@
+// apenas assisti, nao fiz a aula
